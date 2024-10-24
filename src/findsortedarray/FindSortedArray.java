422fa78282de4522df30fd000cd979822d7af2b8
AdilSaeed0347 <adilahmad0347@gmail.com>
2024-10-24 10:21:39 +0500

    Update  FindingSubArrays.java

--- src/findsortedarray/FindSortedArray.java
+++ src/findsortedarray/FindSortedArray.java
@@ -1,11 +1,48 @@
 
- }   
-    
+package findingsubarrays;
+import java.util.Random;
+import java.util.Scanner;
 
- 
+public class FindSorted {
+public void FindingSubArrays(int [] array)
+{   System.out.println("Sub sorted list in array are :");
+    System.out.print("[");
+    for(int i=0; i<array.length-1; i++)
+    {
+        System.out.print(array[i]+" ");
+        if(array[i]>array[i+1])
+        {
+            System.out.print("]");
+            System.out.println("");
+            System.out.print("[");
+        }
+    }
+    System.out.print(array[array.length-1]+"");
+        
+   
+   
+
+
+}
+    
     public static void main(String[] args) {
-       FindSortedArray obj= new FindSortedArray();
+      Random rand= new Random ();
+      Scanner kb= new Scanner(System.in);
+      FindingSubArrays obj= new FindingSubArrays();
+      System.out.println("Enter size of array");
+      int size=kb.nextInt();
+      int [] array = new int [size];
+      for(int i=0; i<size; i++)
+      {array[i]=rand.nextInt(50);}
+        System.out.println("Array element are :");
+      for(int num:array)
+      {System.out.print(num+" ");
+      }
+        System.out.println();
+      obj.FindSubArrays(array);
+      
+      }
       
     }
     
-}
+
